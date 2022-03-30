c3699f57d1aecf62640ef4ede4728e061721fd23
Angel Rodriguez <angelrodri@hotmail.com>
2022-03-30 11:05:12 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function displayColors(colors) {
+    let myColorsHtml = colors.map(color => {
+        return `<div class="my-color" style="background-color: ${color.value}"></div>`
+    }).join('')
+    
+    console.log(myColorsHtml)
+
+    document.body.innerHTML = `<div class="my-colors">
+        ${myColorsHtml}
+    </div>`
+}
+
+async function getColors(colorCount) {
+    let response = await fetch(`https://apis.scrimba.com/hexcolors?count=${colorCount}`)
+    let data = await response.json()
+    let colors = data.colors
+    
+    displayColors(colors)
+}
+
+let colorCount = 100
+getColors(colorCount)
